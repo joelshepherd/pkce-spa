691db7566c1d69c9d5870e326eb77c7b143c9066
Joel Shepherd <joel@swivid.ink>
2021-07-29 18:15:48 +1000

    Improvements to lock

--- src/lock.ts
+++ src/lock.ts
@@ -1,33 +1,34 @@
 type State = [key: string, id: number] | null;
 
 export class Lock {
-  #state: State | null;
+  #state: State;
   #sender: Sender;
 
   constructor(key: string) {
-    // Select messenger type
-    const messenger = "BroadcastChannel" in window ? channel : storage;
-    this.#sender = messenger(key, (state) => (this.#state = state));
+    // Create messenge channel
+    const create = "BroadcastChannel" in window ? broadcast : storage;
+    this.#sender = create(key, (state) => (this.#state = state));
 
     // After registration of the receiver
     this.#state = restoreState(key);
   }
 
   async acquire(key: string): Promise<boolean> {
-    // Check for existing lock of this key
-    if (this.#state && this.#state[0] === key) return false;
+    // Check for an existing lock
+    if (this.#state !== null && this.#state[0] === key) return false;
 
     // Try and acquire the lock
     const id = Math.floor(Math.random() * 100_000_000);
     await this.#sender([key, id]);
 
-    // Check we still control the lock after the safety timeout
-    await delay(100);
+    // Wait long enough that any other thread that has passed the first lock
+    // check above can also reach here
+    await delay(50);
 
-    const stillHasLock =
-      this.#state !== null && this.#state[0] === key && this.#state[1] === id;
-
-    return stillHasLock;
+    // Check we still control the lock
+    return (
+      this.#state !== null && this.#state[0] === key && this.#state[1] === id
+    );
   }
 
   release(): void {
@@ -35,9 +36,45 @@ export class Lock {
   }
 }
 
+/** A channel to send and receive lock messages */
+type Channel = (key: string, receiver: (state: State) => void) => Sender;
+
+type Sender = (state: State) => void;
+
+/** Send lock updates via a broadcast channel (most reliable) */
+const broadcast: Channel = (key, receiver) => {
+  const producer = new BroadcastChannel(key);
+  const consumer = new BroadcastChannel(key);
+
+  consumer.onmessage = (event) => {
+    receiver(event.data);
+  };
+
+  return (state) => {
+    producer.postMessage(state);
+    persistState(key, state);
+  };
+};
+
+/**
+ * Send lock updates via localstorage (least reliable)
+ * Used as a fallback for browsers that do not support broadcast channel
+ */
+const storage: Channel = (key, receiver) => {
+  window.addEventListener("storage", (event) => {
+    if (event.key !== key) return;
+    receiver(restoreState(key));
+  });
+
+  return (state) => {
+    receiver(state);
+    persistState(key, state);
+  };
+};
+
 function restoreState(key: string): State {
   try {
-    const parsed = JSON.parse(window.localStorage.getItem(key) ?? "");
+    const parsed: unknown = JSON.parse(window.localStorage.getItem(key) ?? "");
     if (
       Array.isArray(parsed) &&
       typeof parsed[0] === "string" &&
@@ -57,40 +94,3 @@ function persistState(key: string, state: State): void {
 function delay(timeout: number): Promise<void> {
   return new Promise((res) => setTimeout(res, timeout));
 }
-
-type Sender = (state: State) => Promise<void>;
-type Receiver = (state: State) => void;
-
-type Channel = (key: string, receiver: Receiver) => Sender;
-
-// Send lock updates via a broadcast channel (most reliable)
-const channel: Channel = function (key, receiver) {
-  const producer = new BroadcastChannel(key);
-  const consumer = new BroadcastChannel(key);
-
-  consumer.onmessage = (event) => {
-    receiver(event.data);
-  };
-
-  return async (state) => {
-    producer.postMessage(state);
-    persistState(key, state);
-  };
-};
-
-// Send lock updates via localstorage (least reliable)
-// Used as a fallback for browsers that do not support broadcast channel
-const storage: Channel = function (key, receiver) {
-  window.addEventListener("storage", (event) => {
-    if (event.key !== key) return;
-    receiver(restoreState(key));
-  });
-
-  return async (state) => {
-    // Random wait to protect against concurrent writes that can otherwise break localstorage
-    await delay(Math.random() * 10);
-
-    receiver(state);
-    persistState(key, state);
-  };
-};
